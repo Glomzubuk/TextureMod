aaa3d8e7ac09a96bc4346af061758cc45a8dc20c
Glomzubuk <glomzubuk@gmail.com>
2022-09-14 11:16:30 +0200

    Removed unused code and implemented old code in comments

--- TextureMod/ExchangeClient.cs
+++ TextureMod/ExchangeClient.cs
@@ -29,8 +29,6 @@ namespace TextureMod
         public static void Init()
         {
             var pInfo = TextureMod.Instance.Info;
-            //MessageApi.RegisterCustomMessage(pInfo, (ushort)TexModMessages.TEXMOD_SKINCHECK, TexModMessages.TEXMOD_SKINCHECK.ToString(), ReceiveSkinCheck);
-            //MessageApi.RegisterCustomMessage(pInfo, (ushort)TexModMessages.TEXMOD_SKINCHANGE, TexModMessages.TEXMOD_SKINCHANGE.ToString(), ReceiveCustomSkinChange);
             MessageApi.RegisterCustomMessage(pInfo, (ushort)TexModMessages.TEXMOD_SKINREQUEST, TexModMessages.TEXMOD_SKINREQUEST.ToString(), ReceiveSkinRequest);
             MessageApi.RegisterCustomMessage(pInfo, (ushort)TexModMessages.TEXMOD_SKIN, TexModMessages.TEXMOD_SKIN.ToString(), ReceiveSkin);
             PlayerLobbyState.RegisterPayload(pInfo, OnSendPayload, OnReceivePayload);
@@ -69,7 +67,7 @@ namespace TextureMod
             try
             {
                 SkinHash skinHash = new SkinHash(rawHash);
-                TextureMod.Log.LogDebug("Test: " + skinHash.ToString());
+                TextureMod.Log.LogDebug("Test: " + skinHash);
                 CustomSkinHandler handler = TextureMod.customSkinCache.GetHandlerFromHash(skinHash);
                 if (handler != null)
                 {
@@ -85,48 +83,11 @@ namespace TextureMod
             catch
             { }
         }
-        /*
-        public static void SendSkinCheck(int playerNr)
-        {
-            P2P.SendToPlayerNr(playerNr, new Message((Msg)TexModMessages.TEXMOD_SKINCHECK, P2P.localPeer.playerNr, -1, Encoding.ASCII.GetBytes(skinHash.ToString())));
-        }
 
-
-        public static void ReceiveSkinCheck(Message msg)
-        {
-            if (msg.playerNr == P2P.localPeer.playerNr) return;
-            Hash128 skinHash = Hash128.Parse(Encoding.ASCII.GetString((byte[])msg.ob));
-
-            if (TextureMod.skinCachesHandler.ContainsHash(skinHash))
-            {
-                SendSkin(msg.playerNr, TextureMod.skinCachesHandler[skinHash]);
-            }
-        }
-
-        public static void SendCustomSkinChange(Hash128 skinHash)
-        {
-            P2P.SendOthers(new Message((Msg)TexModMessages.TEXMOD_SKINCHANGE, P2P.localPeer.playerNr, -1, Encoding.ASCII.GetBytes(skinHash.ToString())));
-        }
-
-        public static void ReceiveCustomSkinChange(Message msg)
-        {
-            if (msg.playerNr == P2P.localPeer.playerNr) return;
-            Hash128 skinHash = Hash128.Parse(Encoding.ASCII.GetString((byte[])msg.ob));
-
-            if(!TextureMod.skinCachesHandler.ContainsHash(skinHash))
-            {
-                SendSkinRequest(msg.playerNr, skinHash);
-            }
-            else
-            {
-                GameStates.Send(new Message(Msg.SEL_SKIN, msg.playerNr, (int)TextureMod.skinCachesHandler[skinHash].CharacterVariant));
-            }
-        }
-        */
         private static int transactionIDCounter = 0;
         public static void SendSkinRequest(int playerNrToRequest, SkinHash skinHash)
         {
-            TextureMod.Log.LogDebug("Requesting skin with hash: " + skinHash.ToString());
+            TextureMod.Log.LogDebug("Requesting skin with hash: " + skinHash);
             NetworkApi.SendMessageToPlayer(playerNrToRequest, new Message((Msg)TexModMessages.TEXMOD_SKINREQUEST, P2P.localPeer.playerNr, transactionIDCounter++, skinHash.Bytes, skinHash.Bytes.Length));
         }
 
@@ -134,7 +95,7 @@ namespace TextureMod
         {
             if (msg.playerNr == P2P.localPeer.playerNr) return;
             SkinHash skinHash = new SkinHash((byte[])msg.ob);
-            TextureMod.Log.LogDebug("Received skin request for hash: " + skinHash.ToString());
+            TextureMod.Log.LogDebug("Received skin request for hash: " + skinHash);
             CustomSkin skin = TextureMod.customSkinCache.GetSkinFromHash(skinHash);
             if (skin != null)
             {
@@ -146,7 +107,6 @@ namespace TextureMod
         {
             TextureMod.Log.LogDebug($"Sending skin {skin.Name} to player {playerNrToSendSkin}");
             byte[] skinAsBytes = skin.ToBytes();
-            //TODO That won' t work by default, default packet size limit is too small
             NetworkApi.SendMessageToPlayer(playerNrToSendSkin, new Message((Msg)TexModMessages.TEXMOD_SKIN, P2P.localPeer.playerNr, requestID, skinAsBytes));
         }
 
--- TextureMod/TextureChanger.cs
+++ TextureMod/TextureChanger.cs
@@ -18,8 +18,6 @@ namespace TextureMod
         #region General Fields
         public static string imageFolder = Path.Combine(TextureMod.ResourceFolder, "Images");
 
-
-        //public static bool InMatch => World.instance != null && (DNPFJHMAIBP.HHMOGKIMBNM() == JOFJHDJHJGI.CDOFDJMLGLO || DNPFJHMAIBP.HHMOGKIMBNM() == JOFJHDJHJGI.LGILIJKMKOD) && !LLScreen.UIScreen.loadingScreenActive;
         public string[] debug = new string[20];
         public bool doSkinPost = false;
         public int postTimer = 0;
@@ -67,31 +65,9 @@ namespace TextureMod
         private void FixedUpdate()
         {
             /*
-            if (!neverApplyOpponentsSkin.Value)
-            {
-                if (doSkinPost) { postTimer++; }
-                if (localPlayer != null && localPlayer.customSkin != null)
-                {
-                    if (postTimer >= postTimerLimit)
-                    {
-                        doSkinPost = false;
-                        postTimer = 0;
-                        try
-                        {
-                            ExchangeClient.SendCustomSkinChange(localPlayer.customSkin.SkinHash);
-                        }
-                        catch (Exception ex)
-                        {
-                            Logger.LogError($"Error sending skin change to other players {ex.ToString()}");
-                        }
-                    }
-                }
-            }
-            */
-            /*
             if (silouetteTimer > 0) silouetteTimer--;
             if (reloadCustomSkinTimer > 0) reloadCustomSkinTimer--;*/
-        } //POST and GET requests
+        }
 
         private void Update()
         {
@@ -121,23 +97,11 @@ namespace TextureMod
                     case GameMode.TRAINING:
                     case GameMode.TUTORIAL:
                         #region In training and tutorial
-
-                        if (localPlayer == null)
-                        {
-                            localPlayer = new TexModPlayer(Player.GetPlayer(0), null);
-                        }
-
-                        if (InLobby(GameType.Offline))
-                        {
-                            if (localPlayer.characterModel == null)
-                            {
-                                localPlayer.UpdateModel();
-                            }
-                        }
                         else if (InGame(GameType.Offline))
                         {
                             if (localPlayer.customSkin != null)
                             {
+                                // TODO Reloading the skin library                           
                                 if (Input.GetKeyDown(reloadCustomSkin.Value))
                                 {
                                     try { localPlayer.customSkin.ReloadSkin(); }
@@ -147,32 +111,6 @@ namespace TextureMod
                         }
                         break;
                     #endregion
-                    case GameMode._1v1:
-                    case GameMode.FREE_FOR_ALL:
-                    case GameMode.COMPETITIVE:
-                        #region In ranked and online lobby
-                        if (InLobby(GameType.Any))
-                        {
-                            if (localPlayer == null)
-                            {
-                                localPlayer = new LocalTexModPlayer(Player.GetPlayer(NetworkApi.LocalPlayerNumber), null);
-                            }
-                            IEnumerable<Player> players = Player.GetPlayerList().Where((player) => player != null);
-                            if (Opponents.Count != (players.Count() - 1))
-                            {
-                                foreach (Player player in players)
-                                {
-                                    if (player != null && player.nr != NetworkApi.LocalPlayerNumber)
-                                    {
-                                        tmPlayers[player.nr] = new RemoteTexModPlayer(player);
-                                    }
-                                }
-                            }
-
-                        }
-                        break;
-                }
-                #endregion
             }
             */
         }
@@ -247,121 +185,6 @@ namespace TextureMod
             }
         }
         */
-#if showOLD
-
-        [Obsolete("Method1 is deprecated, please use Method2 instead.", true)]
-        private Texture2D GetLoadedTexture(Character c, Texture2D currentTexture, bool previous, bool random)
-        {
-            Texture2D ret = null;
-            var texname = "";
-
-            bool flipped;
-            if (currentGameMode == GameMode._1v1 && localLobbyPlayer.CJFLMDNNMIE == 1) flipped = true;
-            else flipped = false;
-
-
-            if (random)
-            {
-                var n = rng.Next(0, TextureMod.Instance.tl.characterTextures[c].Count());
-                localLobbyPlayer.CharacterVariant = GetVariantFromFileName(c, TextureMod.Instance.tl.characterTextures[c].ElementAt(n).Key);
-                localLobbyPlayerModel.SetCharacterLobby(localLobbyPlayer.CJFLMDNNMIE, c, GetVariantFromFileName(c, TextureMod.Instance.tl.characterTextures[c].ElementAt(n).Key), flipped);
-                localPlayerCharVar = GetVariantFromFileName(c, TextureMod.Instance.tl.characterTextures[c].ElementAt(n).Key);
-                texname = TextureMod.Instance.tl.characterTextures[c].ElementAt(n).Key;
-                ret = TextureMod.Instance.tl.characterTextures[c].ElementAt(n).Value;
-            }
-            else
-            {
-                if (!previous)
-                {
-                    if (currentTexture == null && TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Value != null)
-                    {
-                        localLobbyPlayer.CharacterVariant = GetVariantFromFileName(c, TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Key);
-                        localLobbyPlayerModel.SetCharacterLobby(localLobbyPlayer.CJFLMDNNMIE, c, GetVariantFromFileName(c, TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Key), flipped);
-                        localPlayerCharVar = GetVariantFromFileName(c, TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Key);
-                        texname = TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Key;
-                        ret = TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Value;
-                    }
-                    else
-                    {
-                        bool retnext = false;
-                        foreach (KeyValuePair<string, Texture2D> pair in TextureMod.Instance.tl.characterTextures[c])
-                        {
-                            if (retnext == true)
-                            {
-                                localLobbyPlayer.CharacterVariant = GetVariantFromFileName(c, pair.Key);
-                                localLobbyPlayerModel.SetCharacterLobby(localLobbyPlayer.CJFLMDNNMIE, c, GetVariantFromFileName(c, pair.Key), flipped);
-                                localPlayerCharVar = GetVariantFromFileName(c, pair.Key);
-                                texname = pair.Key;
-                                ret = pair.Value;
-                                break;
-                            }
-                            else if (retnext == false && currentTexture == pair.Value)
-                            {
-                                retnext = true;
-                                if (currentTexture == TextureMod.Instance.tl.characterTextures[c].Last().Value)
-                                {
-                                    localLobbyPlayer.CharacterVariant = GetVariantFromFileName(c, TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Key);
-                                    localLobbyPlayerModel.SetCharacterLobby(localLobbyPlayer.CJFLMDNNMIE, c, GetVariantFromFileName(c, TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Key), flipped);
-                                    localPlayerCharVar = GetVariantFromFileName(c, TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Key);
-                                    texname = TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Key;
-                                    ret = TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Value;
-                                }
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if (currentTexture == null && TextureMod.Instance.tl.characterTextures[c].ElementAt(0).Value != null)
-                    {
-                        KeyValuePair<string, Texture2D> lastSkin = TextureMod.Instance.tl.characterTextures[c].Last();
-                        localLobbyPlayer.CharacterVariant = GetVariantFromFileName(c, lastSkin.Key);
-                        localLobbyPlayerModel.SetCharacterLobby(localLobbyPlayer.CJFLMDNNMIE, c, GetVariantFromFileName(c, lastSkin.Key), flipped);
-                        localPlayerCharVar = GetVariantFromFileName(c, lastSkin.Key);
-                        texname = lastSkin.Key;
-                        ret = lastSkin.Value;
-                    }
-                    else
-                    {
-                        bool retnext = false;
-                        KeyValuePair<string, Texture2D> firstSkin = TextureMod.Instance.tl.characterTextures[c].First();
-                        KeyValuePair<string, Texture2D> lastSkin = TextureMod.Instance.tl.characterTextures[c].Last();
-
-                        for (int i = TextureMod.Instance.tl.characterTextures[c].Count - 1; i != -1; i--)
-                        {
-                            KeyValuePair<string, Texture2D> pair = TextureMod.Instance.tl.characterTextures[c].ElementAt(i);
-                            if (retnext == true)
-                            {
-                                localLobbyPlayer.CharacterVariant = GetVariantFromFileName(c, pair.Key);
-                                localLobbyPlayerModel.SetCharacterLobby(localLobbyPlayer.CJFLMDNNMIE, c, GetVariantFromFileName(c, pair.Key), flipped);
-                                localPlayerCharVar = GetVariantFromFileName(c, pair.Key);
-                                texname = pair.Key;
-                                ret = pair.Value;
-                                break;
-                            }
-                            else if (retnext == false && currentTexture == pair.Value)
-                            {
-                                retnext = true;
-                                if (currentTexture == firstSkin.Value)
-                                {
-                                    localLobbyPlayer.CharacterVariant = GetVariantFromFileName(c, lastSkin.Key);
-                                    localLobbyPlayerModel.SetCharacterLobby(localLobbyPlayer.CJFLMDNNMIE, c, GetVariantFromFileName(c, lastSkin.Key), flipped);
-                                    localPlayerCharVar = GetVariantFromFileName(c, lastSkin.Key);
-                                    texname = lastSkin.Key;
-                                    ret = lastSkin.Value;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            localPlayerChar = c;
-            texname = CleanTextureName(texname);
-            localSkinNameLabel = texname;
-            return ret;
-        }
-
-#endif
 
         private void GetSkinForUnlocksModel(ScreenUnlocksCharacters screen, sbyte next = 0)
         {
@@ -399,95 +222,7 @@ namespace TextureMod
             */
         }
 
-#if showOLD
-        [Obsolete("Method1 is deprecated, please use Method2 instead.", true)]
-        private Texture2D GetLoadedTextureForUnlocksModel(Texture2D currentTexture, bool previous)
-        {
-            Texture2D texture = null;
-            var texname = "";
-            ScreenUnlocksSkins sus = FindObjectOfType<ScreenUnlocksSkins>();
 
-            if (sus != null)
-            {
-                var curCharactersTextures = TextureMod.Instance.tl.characterTextures[sus.character];
-                if (previous)
-                {
-                    if (currentTexture == null && curCharactersTextures.ElementAt(0).Value != null)
-                    {
-                        sus.ShowCharacter(sus.character, GetVariantFromFileName(sus.character, curCharactersTextures.Last().Key), false);
-                        texname = curCharactersTextures.Last().Key;
-                        texture = curCharactersTextures.Last().Value;
-                    }
-                    else
-                    {
-                        bool retnext = false;
-                        for (int i = curCharactersTextures.Count - 1; i != -1; i--)
-                        {
-                            KeyValuePair<string, Texture2D> pair = curCharactersTextures.ElementAt(i);
-                            if (retnext == true)
-                            {
-                                sus.ShowCharacter(sus.character, GetVariantFromFileName(sus.character, pair.Key), false);
-                                texname = pair.Key;
-                                texture = pair.Value;
-                                break;
-                            }
-                            else if (retnext == false && currentTexture == pair.Value)
-                            {
-                                retnext = true;
-                                if (currentTexture == curCharactersTextures.First().Value)
-                                {
-                                    sus.ShowCharacter(sus.character, GetVariantFromFileName(sus.character, curCharactersTextures.Last().Key), false);
-                                    texname = curCharactersTextures.Last().Key;
-                                    texture = curCharactersTextures.Last().Value;
-                                }
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if (currentTexture == null && curCharactersTextures.ElementAt(0).Value != null)
-                    {
-                        sus.ShowCharacter(sus.character, GetVariantFromFileName(sus.character, curCharactersTextures.ElementAt(0).Key), false);
-                        texname = curCharactersTextures.ElementAt(0).Key;
-                        texture = curCharactersTextures.ElementAt(0).Value;
-                    }
-                    else
-                    {
-                        bool retnext = false;
-                        foreach (KeyValuePair<string, Texture2D> pair in curCharactersTextures)
-                        {
-                            if (retnext == true)
-                            {
-                                sus.ShowCharacter(sus.character, GetVariantFromFileName(sus.character, pair.Key), false);
-                                texname = pair.Key;
-                                texture = pair.Value;
-                                break;
-                            }
-                            else if (retnext == false && currentTexture == pair.Value)
-                            {
-                                retnext = true;
-                                if (currentTexture == curCharactersTextures.Last().Value)
-                                {
-                                    sus.ShowCharacter(sus.character, GetVariantFromFileName(sus.character, curCharactersTextures.ElementAt(0).Key), false);
-                                    texname = curCharactersTextures.ElementAt(0).Key;
-                                    texture = curCharactersTextures.ElementAt(0).Value;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            texname = CleanTextureName(texname);
-
-            siluetteTimer = 5;
-
-            localSkinNameLabel = texname;
-            return texture;
-        }
-
-#endif
 
 
         private void InitConfig()
@@ -517,9 +252,6 @@ namespace TextureMod
             showDebugInfo = config.Bind<bool>("TextureChanger", "showDebugInfo", false);
             config.Bind("TextureChanger", "gap3", "20", "modmenu_gap");
         }
-
-
-
     }
 }
 
